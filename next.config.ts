5e0dd250541594cdda7815ffaeb44884326dfcd2
Masoumeh Firouzzadeh <mfirouzzadeh99@gmail.com>
2025-12-30 23:51:01 +0330

    fix: action

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
+import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
+
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/<REPO_NAME>" : "",
+  images: {
+    unoptimized: true,
+  },
 };
+
+export default withNextIntl(nextConfig);
