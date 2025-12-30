3e32423fce0832c95c7b5660c0599b6578400bd4
Masoumeh Firouzzadeh <mfirouzzadeh99@gmail.com>
2025-12-31 00:30:13 +0330

    feat: Completed

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,9 +1,7 @@
 import { redirect } from "next/navigation";
 
-const BASE_PATH = process.env.NODE_ENV === "production" ? "/next-auth" : "";
-
 function page() {
-  return redirect(`${BASE_PATH}/fa/login`);
+  return redirect("/fa/login");
 }
 
 export default page;
